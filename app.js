e605f7c251c6e8f3410f867611273c012668d1a3
Kanak Tyagi <tyagiknk.india@gmail.com>
2019-05-20 21:24:53 +0530

    learned to read and write file aynchronously

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-var events = require('events');
-var util = require('util');
+var fs = require('fs');
 
-
-var Person = function(name){
-  this.name=name;
-}
-util.inherits(Person, events.EventEmitter);
-
-var james = new Person('james');
-var mary = new Person('mary');
-var ryu = new Person('ryu');
-var people = [james, mary, ryu];
-
-people.forEach(function(person){
-  person.on('speak',function(msg){
-    console.log(person.name + ' said  '+ msg);
-  });
+fs.readFile('readme.txt','utf-8',function(err,data){
+  fs.writeFile('writeMe.txt',data);
 });
-
-james.emit('speak','hi');
+//
