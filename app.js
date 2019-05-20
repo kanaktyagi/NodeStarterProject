0dd7e8950dfd4deeb902d9bf2c56ea67e34b6033
Kanak Tyagi <tyagiknk.india@gmail.com>
2019-05-20 20:47:43 +0530

    changed app.js used custom eventemitter

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
+var events = require('events');
+var util = require('util');
 
-var stuff = require('./stuff');
-console.log(stuff.counter(['hi','bye','say']));
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(stuff.pi,4));
+
+var Person = function(name){
+  this.name=name;
+}
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('james');
+var mary = new Person('mary');
+var ryu = new Person('ryu');
+var people = [james, mary, ryu];
+
+people.forEach(function(person){
+  person.on('speak',function(msg){
+    console.log(person.name + ' said  '+ msg);
+  });
+});
+
+james.emit('speak','hi');
