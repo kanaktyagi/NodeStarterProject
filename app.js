24b31f442330eefe53fb8ad04c74653ff347a534
Kanak Tyagi <tyagiknk.india@gmail.com>
2019-05-20 21:39:22 +0530

    unlinked the file and then deleted directory aynchronously

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 
 //fs.unlink('writeMe.txt');
-fs.mkdir('stuff',function(){
-  fs.readFile('readme.txt','utf-8', function(err, data){
-    fs.writeFile('./stuff/writeMe.txt', data);
-  });
+fs.unlink('./stuff/writeMe.txt', function(){
+    fs.rmdir('stuff');
 });
